7176106c6686091bde9a3a2ed353bde0dcf582fd
kingstar <admin@baks.dev>
2025-01-23 16:16:53 +0300

    Baks Development

--- Resources/assets/avito-promotion/company.js
+++ Resources/assets/avito-promotion/company.js
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -57,9 +57,6 @@ executeFunc(function initAvitoPromotion()
     const currentDeleteBtns = document.getElementById('filterCollection').querySelectorAll('.del-item-filter');
 
     if(currentDeleteBtns)
-    {
-        Ni
-    }
     {
         deleteItem(currentDeleteBtns)
     }
