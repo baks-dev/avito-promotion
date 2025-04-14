df05c259e996e2bd834c385b272ad85acd0df649
kingstar <admin@baks.dev>
2025-04-14 20:01:07 +0300

    Baks Development

--- Controller/Admin/Tests/EditControllerTest.php
+++ Controller/Admin/Tests/EditControllerTest.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -19,7 +19,6 @@
  *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  *  THE SOFTWARE.
- *
  */
 
 namespace BaksDev\Avito\Promotion\Controller\Admin\Tests;
@@ -59,6 +58,12 @@ final class EditControllerTest extends WebTestCase
         $main = $em
             ->getRepository(AvitoPromotion::class)
             ->find(AvitoPromotionUid::TEST);
+
+        if(empty($main))
+        {
+            self::assertNull($main);
+            return;
+        }
 
         self::assertNotNull($main);
 
